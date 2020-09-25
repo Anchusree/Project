e08c7606c5d24f0cd3035723809d3771b94a3b0a
Anchusree <anchusreen@gmail.com>
2020-09-25 20:30:38 +0300

    Updates

---
+++ precache-manifest.2d1863e57699fcb3eda72b6674777ba7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea0f1af6918be5c1003628b6481bc0c",
+    "url": "/Project/index.html"
+  },
+  {
+    "revision": "47d2d8bcf29e0faf3f22",
+    "url": "/Project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bbd7a6a2ae9be40b4410",
+    "url": "/Project/static/css/main.3d2f421d.chunk.css"
+  },
+  {
+    "revision": "47d2d8bcf29e0faf3f22",
+    "url": "/Project/static/js/2.857fb3ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Project/static/js/2.857fb3ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd7a6a2ae9be40b4410",
+    "url": "/Project/static/js/main.bee858d5.chunk.js"
+  },
+  {
+    "revision": "b18372ac4ff8d4982348",
+    "url": "/Project/static/js/runtime-main.8f085fc7.js"
+  },
+  {
+    "revision": "ff92d82e831ada49096e097975a6b38c",
+    "url": "/Project/static/media/GBI-logo.ff92d82e.jpg"
+  },
+  {
+    "revision": "304a390accf9c7e75989f4c3b458fc11",
+    "url": "/Project/static/media/contactjpg.304a390a.jpg"
+  },
+  {
+    "revision": "59e5c7d92e2a0704f90281ba19030c84",
+    "url": "/Project/static/media/doha.59e5c7d9.jpg"
+  },
+  {
+    "revision": "c7c5c8870313c39c2988087e99f36368",
+    "url": "/Project/static/media/getconnected.c7c5c887.jpg"
+  },
+  {
+    "revision": "23fb2a7976b88575485b8a68864754a4",
+    "url": "/Project/static/media/internet-web.23fb2a79.jpg"
+  },
+  {
+    "revision": "a1173547378144e444c9f0c663d1429d",
+    "url": "/Project/static/media/joinus.a1173547.jpg"
+  },
+  {
+    "revision": "5426eb20c20b5d3e59804a5493ae04ac",
+    "url": "/Project/static/media/ooredoo.5426eb20.jpg"
+  },
+  {
+    "revision": "da6c76aea631268819be05c58f9773f0",
+    "url": "/Project/static/media/qatar1.da6c76ae.jpg"
+  },
+  {
+    "revision": "e3d53cf8ace3925b851f1686226bc43e",
+    "url": "/Project/static/media/qix_logo.e3d53cf8.jpg"
+  },
+  {
+    "revision": "e8fabb93a1382d3b166cfaa38495aa3b",
+    "url": "/Project/static/media/vodafone-logo.e8fabb93.jpg"
+  }
+]);
